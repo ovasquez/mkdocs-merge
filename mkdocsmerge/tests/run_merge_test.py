d50406a9bbc37817d970252c542207efa4061f13
Mihai Popescu <mihai@unity3d.com>
2018-05-29 12:28:52 +0200

    make flake8 happy

--- mkdocsmerge/tests/run_merge_test.py
+++ mkdocsmerge/tests/run_merge_test.py
@@ -26,27 +26,27 @@ class TestRunMerge(unittest.TestCase):
             yml = make_simple_yaml(site_name, docs_dir)
             generate_website(self.tmpdir, site_name, yml)
 
-        merged_pages = mkdocsmerge.merge.run_merge(site_names[0], site_names[1:],
-            True, lambda x: None)
+        merged_pages = mkdocsmerge.merge.run_merge(
+            site_names[0], site_names[1:], True, lambda x: None)
 
         for site_name in site_names[1:]:
-            index_path = os.path.join(site_name,
-                docs_dir_map.get(site_name, 'docs'), 'index.md')
+            index_path = os.path.join(
+                site_name, docs_dir_map.get(site_name, 'docs'), 'index.md')
             self.assertTrue(os.path.exists(index_path))
 
-            docs_dir_path = os.path.join(site_names[0],
-                docs_dir_map.get(site_names[0], 'docs'), '%s_website' % site_name.lower())
+            docs_dir_path = os.path.join(
+                site_names[0], docs_dir_map.get(site_names[0], 'docs'),
+                '%s_website' % site_name.lower())
             self.assertTrue(os.path.exists(docs_dir_path))
 
-        self.assertEqual(merged_pages,
-        {
+        self.assertEqual(merged_pages, {
             'site_name': '__master__ Website',
             'pages': [
                 {'Home': "index.md"},
                 {'Foo Website': [
                     {'Home': 'foo_website/index.md'}
                 ]},
-                {'Bar Website' : [
+                {'Bar Website': [
                     {'Home': 'bar_website/index.md'}
                 ]},
                 {'Test Website': [
